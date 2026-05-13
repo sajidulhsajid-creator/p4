c97975f862f0d741b19f938715b5ea46adc50450
Sajid Haque <sajidul.h.sajid@gmail.com>
2026-05-13 12:58:27 +1000

    Change base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // base: "./" makes the built site work from any GitHub Pages repository path.
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/p4/",
 });
